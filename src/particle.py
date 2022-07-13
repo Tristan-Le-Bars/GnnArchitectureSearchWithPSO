e1209c7f8cc43881e0962ca32ac4d6ef0bbb708a
Tristan-Le-Bars <tristan.le-bars@epitech.eu>
2022-07-13 15:05:01 +0200

    modifying particle objet

--- src/particle.py
+++ src/particle.py
@@ -5,17 +5,16 @@ import copy
 
 class Particle:
     def __init__(self):
-        #
-        self.weightPosition = []
-        self.weightVelocity = []
-        self.biasPosition = []
-        self.biasVelocity = []
+        self.hidden_dim = 1
+        self.hidden_num = 1
         self.parameters = NN_parameters()
         self.cognitiveCoef = 1 # can be changed
         self.socialCoef = 1 # can be changed
         self.informantList = []
         self.informants_best_err = -1
         self.best_err = -1
+        self.velocity_hidden_num = random.random()
+        self.velocity_hidden_dim = random.random()
 
     def setInformants(self, swarm, informantNum, index):
         banned_index = []
@@ -38,48 +37,24 @@ class Particle:
 
     def update_velocity(self):
         inertia_weight = 1
-        for i in range(CONNECTION_NUMBER):
-            r1 = np.random.randn(4, self.num_neurons)
-            r2 = np.random.randn(4, self.num_neurons)
 
-            vel_cog = self.cognitive * r1 * (self.best_wb[0] - WEIGHT_LIST[0])      # find how to obtain the synapse weights from pytorch
-            vel_soc = self.social * r2 * (self.informants_best[0] - WEIGHT_LIST[0]) # find how to obtain the synapse weights from pytorch
-            self.velW1 = inertia_weight * self.velW1 + vel_soc + vel_cog
+        r1 = random.random()
+        r2 = random.random()
 
-            #Change the velocity values for B1
-            r1 = np.random.randn(1, self.num_neurons)
-            r2 = np.random.randn(1, self.num_neurons)
+        vel_cog = self.cognitive * r1 * (self.best_wb[0])
+        vel_soc = self.social * r2 * (self.informants_best[0])
+        self.velocity_hidden_num = inertia_weight * self.velocity_hidden_num + vel_soc + vel_cog
 
-            vel_cog = self.cognitive * r1 * (self.best_wb[1] - self.B1)
-            vel_soc = self.social * r2 * (self.informants_best[1] - self.B1)
-            self.velB1 = inertia_weight * self.velB1 + vel_soc + vel_cog
+        #Change the velocity values for B1
+        r1 = random.random()
+        r2 = random.random()
 
+        vel_cog = self.cognitive * r1 * (self.best_wb[1])
+        vel_soc = self.social * r2 * (self.informants_best[1])
+        self.velocity_hidden_dim = inertia_weight * self.velocity_hidden_dim + vel_soc + vel_cog
 
-
-
-
-
-        CONNECTION_NUMBER = self.parameters.layerNumber - 1 + 2 # -1 hidden connection + 2 input and output connections
-        randomVelocity = []
-
-        #Change the velocity values for W1
-
+    # Update the weights and biases
+    def change_wb(self):
+            self.velocity_hidden_num = self.velocity_hidden_num + self.hidden_num
+            self.velocity_hidden_dim = self.velocity_hidden_dim + self.hidden_dim
             
-
-        
-
-        # Change the velocity values for W2
-        r1 = np.random.randn(self.num_neurons,1)
-        r2 = np.random.randn(self.num_neurons,1)
-
-        vel_cog = self.cognitive * r1 * (self.best_wb[2] - self.W2)
-        vel_soc = self.social * r2 * (self.informants_best[2] - self.W2)
-        self.velW2 = inertia_weight * self.velW2 + vel_soc + vel_cog
-
-        # Change the velocity values for B2
-        r1 = np.random.randn()
-        r2 = np.random.randn()
-
-        vel_cog = self.cognitive * r1 * (self.best_wb[3] - self.B2)
-        vel_soc = self.social * r2 * (self.informants_best[3] - self.B2)
-        self.velB2 = inertia_weight * self.velB2 + vel_soc + vel_cog
